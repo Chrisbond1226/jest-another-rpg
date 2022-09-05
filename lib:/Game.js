e616e58b141572e848b70c918a756d2d5f7aa6e2
Christofer Bond <chrisbond66@gmail.com>
2022-09-04 23:50:06 -0700

    added more game logic like battles

--- lib:/Game.js
+++ lib:/Game.js
@@ -1,5 +1,4 @@
 const inquirer = require("inquirer");
-const { relativeTimeThreshold } = require("moment");
 const Enemy = require("./Enemy");
 const Player = require("./Player");
 
@@ -15,6 +14,7 @@ Game.prototype.initializeGame = function () {
   this.enemies.push(new Enemy("goblin", "sword"));
   this.enemies.push(new Enemy("orc", "baseball bat"));
   this.enemies.push(new Enemy("skeleton", "axe"));
+
   this.currentEnemy = this.enemies[0];
 
   inquirer
@@ -45,7 +45,6 @@ Game.prototype.startNewBattle = function () {
 
 Game.prototype.battle = function () {
   if (this.isPlayerTurn) {
-    // player prompts will go here
     inquirer
       .prompt({
         type: "list",
@@ -55,11 +54,11 @@ Game.prototype.battle = function () {
       })
       .then(({ action }) => {
         if (action === "Use potion") {
-          // follow-up prompt will go here
           if (!this.player.getInventory()) {
             console.log("You don't have any potions!");
-            return;
+            return this.checkEndOfBattle();
           }
+
           inquirer
             .prompt({
               type: "list",
@@ -73,15 +72,50 @@ Game.prototype.battle = function () {
               const potionDetails = action.split(": ");
               this.player.usePotion(potionDetails[0] - 1);
               console.log(`You used a ${potionDetails[1]} potion.`);
+              this.checkEndOfBattle();
             });
         } else {
           const damage = this.player.getAttackValue();
           this.currentEnemy.reduceHealth(damage);
 
-          console.log(`You were attacked by the ${this.currentEnemy.name}`);
+          console.log(`You attacked the ${this.currentEnemy.name}`);
           console.log(this.currentEnemy.getHealth());
+
+          this.checkEndOfBattle();
         }
       });
+  } else {
+    const damage = this.currentEnemy.getAttackValue();
+    this.player.reduceHealth(damage);
+
+    console.log(`You were attacked by the ${this.currentEnemy.name}`);
+    console.log(this.player.getHealth());
+
+    this.checkEndOfBattle();
+  }
+};
+Game.prototype.checkEndOfBattle = function () {
+  if (this.player.isAlive() && this.currentEnemy.isAlive()) {
+    this.isPlayerTurn = !this.isPlayerTurn;
+    this.battle();
+  } else if (this.player.isAlive() && !this.currentEnemy.isAlive()) {
+    console.log(`You've defeated the ${this.currentEnemy.name}`);
+
+    this.player.addPotion(this.currentEnemy.potion);
+    console.log(
+      `${this.player.name} found a ${this.currentEnemy.potion.name} potion`
+    );
+
+    this.roundNumber++;
+
+    if (this.roundNumber < this.enemies.length) {
+      this.currentEnemy = this.enemies[this.roundNumber];
+      this.startNewBattle();
+    } else {
+      console.log("Yout win!");
+    }
+  } else {
+    console.log("you've been deafeated!");
   }
 };
 module.exports = Game;
